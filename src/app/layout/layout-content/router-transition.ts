1e9ca9af782ae8f6514e3de750fa0ca5520b99e1
Truong Nguyen Hoang Anh <hoanganh727018@gmail.com>
2023-11-12 23:28:08 +0700

    Finalised transition navigation

--- src/app/layout/layout-content/router-transition.ts
+++ src/app/layout/layout-content/router-transition.ts
@@ -1,4 +1,4 @@
-import {trigger, animate, style, group, query, transition} from '@angular/animations';
+import {trigger, animate, style, group, query, transition, animateChild} from '@angular/animations';
 
 export const routerTransition = trigger('routerTransition', [
     transition('* <=> *', [    
@@ -6,11 +6,11 @@ export const routerTransition = trigger('routerTransition', [
         group([ 
           query(':enter', [
             style({ transform: 'translateX(100%)' }),
-            animate('0.5s ease-in-out', style({ transform: 'translateX(0%)' }))
+            animate('0.6s ease-in-out', style({ transform: 'translateX(0%)' }))
           ]),
           query(':leave', [
             style({ transform: 'translateX(0%)' }),
-            animate('0.5s ease-in-out', style({ transform: 'translateX(-100%)' }))]),
-        ])
+            animate('0.6s ease-in-out', style({ transform: 'translateX(-200%)' }))]),
+        ]),
       ])
 ]);
